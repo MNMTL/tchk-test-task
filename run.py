e7ab05de3a7cc57aa297ff66aeb72d7c1480eb6a
Semen Ivanov <relazer93@gmail.com>
2025-04-23 21:41:30 +0500

    2.0

--- run.py
+++ run.py
@@ -2,23 +2,21 @@ import json
 from datetime import datetime as dt
 from datetime import timedelta
 
-def check_capacity(max_capacity: int, guests: list) -> bool:
-    start_date = min(set([dt.strptime(i['check-in'], '%Y-%m-%d') for i in guests]))  # дата заезда первого клиента
-    end_date = max(set([dt.strptime(i['check-out'], '%Y-%m-%d') for i in guests]))  # дата последнего выселения
-    date_dict = {dt.strftime((start_date + timedelta(n)), '%Y-%m-%d'): 0 for n in
-                 range(int((end_date - start_date).days) + 1)}
 
+def check_capacity(max_capacity: int, guests: list) -> bool:
+    date_dict = {}
     for guest in guests:
         check_in = dt.strptime(guest['check-in'], '%Y-%m-%d')
         check_out = dt.strptime(guest['check-out'], '%Y-%m-%d')
         if (check_out - check_in).days == 0:
             date_dict[guest['check-in']] = 1
         else:
-            for d in [dt.strptime(guest["check-in"], '%Y-%m-%d') + timedelta(n) for n in
-                      range((dt.strptime(guest["check-out"], '%Y-%m-%d') - dt.strptime(guest["check-in"],
-                                                                                       '%Y-%m-%d')).days)]:
-                date_dict[dt.strftime(d, '%Y-%m-%d')] += 1
-
+            for d in [check_in + timedelta(n) for n in range((check_out - check_in).days)]:
+                delta = d.strftime('%Y-%m-%d')
+                if delta in date_dict:
+                    date_dict[delta] += 1
+                else:
+                    date_dict[delta] = 1
     if max_capacity >= max(date_dict.values()):
         return True
     else:
